129958aa5e98f68f8c91a2160eecf5b7a2dce4eb
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:20:36 +0530

    Find the customer who has ordered the most ‘Chai’ product

--- Question26.sql
+++ Question26.sql
@@ -4,4 +4,4 @@ inner join orders on customers.customer_id=orders.customer_id
 inner join order_details on orders.order_id=order_details.order_id
 inner join products on order_details.product_id=products.product_id
 where product_name='Chai'
-limit 1
+limit 1  
