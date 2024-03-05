ade169a56c9718b3d75f485acd9a1a2bb55a1d6b
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:18:10 +0530

    Find the customers who have spent more than $5000 in total

--- Question19.sql
+++ Question19.sql
@@ -4,4 +4,4 @@ inner join orders on customers.customer_id=orders.customer_id
 inner join order_details on orders.order_id=order_details.order_id
 inner join products on order_details.product_id=products.product_id
 where totalcost>5000
-order by totalcost DESC;
+order by totalcost DESC; 
