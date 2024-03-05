05b62aeb38562d7587283d24cd7268db97a83ccd
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:16:26 +0530

    .Find the most expensive product in each category

--- Question14.sql
+++ Question14.sql
@@ -4,4 +4,4 @@ inner join categories on categories.category_id=products.category_id
 where products.unit_price =
 (select max(unit_price) 
  from products
- where categories.category_id=products.category_id)
+ where categories.category_id=products.category_id)  
