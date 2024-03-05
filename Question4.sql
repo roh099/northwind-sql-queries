3c8f0d701580e943906b68ceafa3d2e92f9d3403
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:12:28 +0530

    Find the total sales (Quantity*Unit_Price) for each category of products

--- Question4.sql
+++ Question4.sql
@@ -4,3 +4,4 @@ from order_details
 inner join products on order_details.product_id=products.product_id
 inner join categories on products.category_id=categories.category_id
 group by category_name
+
