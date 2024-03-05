cb68e9a27f80d05d395f2c6e630c8d37fbb91969
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:21:55 +0530

    Find the top 3 most popular categories of products ordered

--- Question28.sql
+++ Question28.sql
@@ -4,4 +4,4 @@ inner join products on order_details.product_id=products.product_id
 inner join categories on products.category_id=categories.category_id
 group by  category_name
 order by most_popular_orders desc
-limit 3
+limit 3  
