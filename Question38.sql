7b1369bca879d9f908e64a61b534f21b13985451
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:25:59 +0530

    Classify employees based on the number of orders they have processed such that NumberOfOrders > 100 Then PerformanceCategory as ‘High Performing’. If NumberOfOrders>50 Then ‘Medium Performing’ and else ‘Lower Performing’

--- Question38.sql
+++ Question38.sql
@@ -2,7 +2,7 @@ select employees.first_name,employees.last_name,count(orders.order_id) AS Number
 case
 when count(orders.order_id)>100 then 'High_Performing'
 when count(orders.order_id)>50 then 'medium_Performing'
-else 'lower_Performing'
+else 'lower_Performing' 
 end as performance_category
 from employees
 inner join orders on employees.employee_id=orders.employee_id
