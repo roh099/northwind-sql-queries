56e672bff5e3c451c54a9058500b476d0bf4ddfb
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:14:26 +0530

    Find the customers who have not placed any orders

--- Question9.sql
+++ Question9.sql
@@ -2,4 +2,4 @@ select contact_name
 from customers
 left join orders on customers.customer_id=orders.customer_id
 --inner join order_details on orders.order_id=order_details.order_id
-where order_id is null;
+where order_id is null;  
