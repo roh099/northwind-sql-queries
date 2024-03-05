7d4653f26a8da7f16b4bca2164e52a0263ed0fe1
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:19:05 +0530

    Get the list of customers who have ordered ‘Chai’ product

--- Question21.sql
+++ Question21.sql
@@ -3,4 +3,4 @@ from customers
 inner join orders on customers.customer_id=orders.customer_id
 inner join order_details on orders.order_id=order_details.order_id
 inner join products on order_details.product_id=products.product_id
-where product_name='Chai'
+where product_name='Chai'  
