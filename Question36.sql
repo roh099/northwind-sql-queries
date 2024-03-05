5cd611bf00823d1b2d649e4bce3e4b19872313b9
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:25:19 +0530

    Classify customers based on their total order amounts such that total order amounts > 5000 should be classified as ‘High Value’, if > 1000 then as ‘Medium Value’ and otherwise as ‘Low Value’

--- Question36.sql
+++ Question36.sql
@@ -7,5 +7,5 @@ end as customer_class
 from customers
 inner join orders on customers.customer_id=orders.customer_id
 inner join order_details on orders.order_id=order_details.order_id
-inner join products on order_details.product_id=products.product_id
+inner join products on order_details.product_id=products.product_id 
 GROUP BY customers.customer_id, customers.company_name;
