28d50ac94112ac6c5af59cdeb8a508010bdd9fa0
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:24:01 +0530

    Find the suppliers who supply the top 5 most sold products

--- Question33.sql
+++ Question33.sql
@@ -3,4 +3,4 @@ inner join products on products.supplier_id=suppliers.supplier_id
 inner join order_details on order_details.product_id=products.product_id
 group by contact_name
 order by Total_sold_products desc
-limit 5;
+limit 5;  
