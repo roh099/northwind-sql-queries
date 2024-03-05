64e9fc52da5f7db682d1a152cc8e90476d60b43d
Rohit Thakur <98614397+roh099@users.noreply.github.com>
2024-03-05 13:25:02 +0530

    Find the total sales for each year

--- Question35.sql
+++ Question35.sql
@@ -3,4 +3,4 @@ FROM orders
 INNER JOIN order_details ON orders.order_id = order_details.order_id
 inner join products on order_details.product_id=products.product_id
 WHERE YEAR(order_date)
-group by YEAR(order_date)
+group by YEAR(order_date) 
